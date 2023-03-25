371fbd3c3ed3809f0ea3581c82513a9e97867158
Matheus Araujo <matheus.saraujo@gmail.com>
2023-03-25 11:15:38 -0300

    feat: initial config (#2)
    * feat: #1 semantic-release, husky and gha
    
    * feat: #1 empty console application
    
    * feat: #1 empty web application
    
    * feat: #1 semantic-release

---
+++ RickAndMorty.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RickAndMorty.WebApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
